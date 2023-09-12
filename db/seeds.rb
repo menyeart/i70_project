cff555774b10d4c82ebdfb05128c5a8994911a42
Matt Enyeart <menyeart1@gmail.com>
2023-09-12 09:00:13 -0600

    Add initial ski areas to databse seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+loveland = SkiArea.create!(name: "Loveland", location_id: "ChIJDW4E2OtRaocR6Ny-eVsZRyY" )
+winter_park = SkiArea.create!(name: "Winter Park", location_id: "ChIJI0qcWKXKa4cRFu-NV44W0Zo" )
+arapahoe_basin = SkiArea.create!(name: "Arapahoe Basin", location_id: "ChIJWd2U7rRQaocRllxVIFIp5Ts" )
+keystone = SkiArea.create!(name: "Keystone", location_id: "ChIJW_aKIwJaaocRr6sP37eZJ_M" )
+breckenridge = SkiArea.create!(name: "Breckendridge", location_id: "ChIJ5SL_Vd71aocRHD59U1wlA8s" )
+copper = SkiArea.create!(name: "Copper", location_id: "ChIJe2Rv9PdfaocR-3_TarNZh2M")
+vail = SkiArea.create!(name: "Vail", location_id: "ChIJ29swxmBwaocRoq1FSy3Pwqc" )
+beaver_creek = SkiArea.create!(name: "Beaver Creek", location_id: "ChIJLUeXah54aocRLCubAYD6EBI" )
+cooper = SkiArea.create!(name: "Cooper", location_id: "ChIJYbUu1wOPaocRTDe7TMTsUhE")
+monarch = SkiArea.create!(name: "Monarch", location_id: "ChIJtzbUKLpCFYcRGpheZl7SdZw")
