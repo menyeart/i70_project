8143746633253d7bf4dd8312d2844f74efd1cd03
Matt Enyeart <menyeart1@gmail.com>
2023-09-19 07:57:47 -0600

    Add code for rendering/serialzing JSON

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     forecast = ForecastFacade.new(params[:direction]).create_forecast
+    render json: ForecastSerializer.new(forecast)
   end
 end
