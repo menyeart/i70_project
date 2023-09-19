0e90e08d4ef38339bb038e24123c88eab7cc6ac6
Matt Enyeart <menyeart1@gmail.com>
2023-09-19 08:01:34 -0600

    Complete request test by adding additional requires for forecast object

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "Forecast request spec", type: :request do
   describe "#index", :vcr do
     before(:each) do
-      time = Time.local(2023, 9, 16, 4, 0, 0)
+      time = Time.local(2024, 9, 16, 4, 0, 0)
       Timecop.travel(time)
     
       loveland = SkiArea.create!(name: "Loveland", location_id: "ChIJDW4E2OtRaocR6Ny-eVsZRyY" )
       winter_park = SkiArea.create!(name: "Winter Park", location_id: "ChIJy0LIeaDKa4cRW2sSTH03-bU" )
       arapahoe_basin = SkiArea.create!(name: "Arapahoe Basin", location_id: "ChIJWd2U7rRQaocRllxVIFIp5Ts" )
       keystone = SkiArea.create!(name: "Keystone", location_id: "ChIJW_aKIwJaaocRr6sP37eZJ_M" )
-      breckenridge = SkiArea.create!(name: "Breckendridge", location_id: "ChIJ5SL_Vd71aocRHD59U1wlA8s" )
+      breckenridge = SkiArea.create!(name: "Breckenridge", location_id: "ChIJ5SL_Vd71aocRHD59U1wlA8s" )
       copper = SkiArea.create!(name: "Copper", location_id: "ChIJe2Rv9PdfaocR-3_TarNZh2M")
       vail = SkiArea.create!(name: "Vail", location_id: "ChIJ29swxmBwaocRoq1FSy3Pwqc" )
       beaver_creek = SkiArea.create!(name: "Beaver Creek", location_id: "ChIJLUeXah54aocRLCubAYD6EBI" )
@@ -21,10 +21,15 @@ RSpec.describe "Forecast request spec", type: :request do
       it "returns a duration forecast with the specfied attributes" do
         get "/api/v1/forecast?direction=west"
         forecast = JSON.parse(response.body, symbolize_names: true)
- 
+        attribute_keys = [:date, :direction, :duration_info]
+        resort_keys = [:Loveland, :"Winter Park", :"Arapahoe Basin", :Keystone, :Breckenridge, :Copper, :Vail, :"Beaver Creek", :Cooper, :Monarch]
+      
         expect(response).to be_successful
         expect(forecast).to be_a(Hash)
         expect(forecast).to have_key(:data)
+        expect(forecast[:data][:attributes].keys).to eq(attribute_keys)
+        expect(forecast[:data][:attributes][:direction]).to eq("west")
+        expect(forecast[:data][:attributes][:duration_info].keys).to eq(resort_keys)
     end
   end
 end
