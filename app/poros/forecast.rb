c9eee3d6caedaf8c9350ce7dfc2975389c2ca384
Matt Enyeart <menyeart1@gmail.com>
2023-09-19 07:59:05 -0600

    Add id to satisfy JSON serializer requirement

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,5 +1,7 @@
 class Forecast
-  def initialize(date, direction, duration_info)
+  attr_reader :direction, :date, :duration_info, :id
+  def initialize(direction, duration_info)
+    @id = 1
     @date = Date.today
     @direction = direction
     @duration_info = duration_info
